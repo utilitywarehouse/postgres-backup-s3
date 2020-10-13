0793faca8a3f0faa4773fc2fbb1b6f3e176a52b1
phewitt-uw <phewitt@uw.co.uk>
2020-10-13 15:53:18 +0100

    initial upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+# exit if a command fails
+set -e
+
+
+apk update
+
+# install s3 tools
+apk add python3 py3-pip
+pip3 install awscli
+
+# install go-cron
+apk add curl
+curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
+chmod u+x /usr/local/bin/go-cron
+apk del curl
+
+
+# cleanup
+rm -rf /var/cache/apk/*
